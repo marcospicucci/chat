7288abfe40db147f0d261ee083dd6aa51faa918f
marcospicucci <marcos.picucci@gmail.com>
2022-07-26 14:54:07 -0300

    agregando

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express");
-const app = express()
+const app = express();
+const http = require('http').createServer(app);
+const io = require("socket.io")(http);
 
-const io = require("socket.io");
+require('dotenv').config();
+process.env.PORT; //3000
+process.env.NODE_ENV; //development
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.get("/", function(req, res){
+    res
+	.status(200)
+	.sendFile(__dirname + "/public/home.html");
+});
+
+http.listen(port, function(){
+    console.log("Escuchando en el puerto " + port);
+});
